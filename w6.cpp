edafbd269d7d6c16c58a9112995105e5c77ac395
GyalGyal <siyul13gm@gmail.com>
2025-01-18 14:07:58 +0900

    Add w6

---
+++ w6.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+	
+int main()
+{
+	int a, b;
+	float c, d, bmi;
+
+	scanf_s("%d %d", &a, &b);
+
+	c = (a + 10) / 100.000;
+	d = b - 5;
+
+	bmi = d / (c * c);
+
+	printf("BMI = %f", bmi);
+
+
+
+	return 0;
+
+
+
+
+
+}
