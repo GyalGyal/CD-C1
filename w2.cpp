cd3b909fdd870fcf8e7cd459cc79ac201bde7fe4
GyalGyal <siyul13gm@gmail.com>
2025-01-18 14:12:29 +0900

    Add w2

---
+++ w2.cpp
@@ -0,0 +1,28 @@
+﻿
+
+
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+	int main()
+
+{
+
+	 printf("오늘 날짜는 %d 년 %d 월 %d 일 입니다.\n", 2024, 12, 14);
+
+	 printf("현재 시각은 오후 %d 시 %d 분 %d 초 입니다.\n", 2, 35, 15);
+
+	 printf("밑변이 %d%s, 높이가 %d%s인 삼각형의 넓이는 %d%s입니다\n", 3, "cm", 4, "cm", 6, "제곱센티미터");
+
+	 printf("지름이 %d%s인 원의 넓이는 %f제곱센티미터, 둘레는 %f%s입니다\n", 9, "cm", pow(9/2, 2) * M_PI, 9*M_PI, "cm");
+
+	 printf("%d%c는 %0.3f%s입니다\n", 12378, 'm', 123478/1000.0, "km");
+
+
+
+	
+
+	
+
+
+}
