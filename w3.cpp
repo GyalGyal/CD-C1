cd5ce15e63cb1d0bbf52f163a19774aff1ac362b
GyalGyal <siyul13gm@gmail.com>
2025-01-18 14:19:40 +0900

    Delete w3.cpp

--- w3.cpp
+++
@@ -1,9 +0,0 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>  
-void main()
-{
-	float distance, weight;
-	distance = 35.53; weight = 6.77;
-	scanf("%fm %fkg", distance, weight);
-	printf("%fm %fkg", distance, weight);
-}
