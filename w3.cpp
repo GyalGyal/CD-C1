0b380709fa0c38a598544f8c0871e163ec545d2e
GyalGyal <siyul13gm@gmail.com>
2025-01-18 14:18:21 +0900

    Add 3

---
+++ w3.cpp
@@ -0,0 +1,9 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>  
+void main()
+{
+	float distance, weight;
+	distance = 35.53; weight = 6.77;
+	scanf("%fm %fkg", distance, weight);
+	printf("%fm %fkg", distance, weight);
+}
